980e0747cd7e2df314f55a5f2e93699868239faf
AnshikaOel <119701265+AnshikaOel@users.noreply.github.com>
2023-12-03 20:41:37 +0000

    Added route for FAQs

--- routes/index.js
+++ routes/index.js
@@ -6,12 +6,16 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.get('/hello', function(req, res, next) {
-    var name = {
-        name:"name",
-        age:15
-    }
-    res.json(name);
+router.get('/faqs', function(req, res, next) {
+    var faqs = [{
+        question:"question1",
+        answer:"answer1"
+    },
+    {
+      question:"question2",
+      answer:"answer2"
+  }]
+    res.json(faqs);
 });
 
 module.exports = router;
