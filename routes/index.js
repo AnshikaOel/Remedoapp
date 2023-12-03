1d7a3581644bad2d7f52f51ce78b798844f36b49
AnshikaOel <119701265+AnshikaOel@users.noreply.github.com>
2023-12-03 20:31:26 +0000

    Init express server

---
+++ routes/index.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.get('/hello', function(req, res, next) {
+    var name = {
+        name:"name",
+        age:15
+    }
+    res.json(name);
+});
+
+module.exports = router;
